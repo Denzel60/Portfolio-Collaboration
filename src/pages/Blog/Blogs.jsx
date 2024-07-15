97d45494c8b9d23136a634484b3212d03884b19a
Your name <your_email@example.com>
2024-07-15 16:00:00 +0300

    creates the blog page

--- src/pages/Blog/Blogs.jsx
+++ src/pages/Blog/Blogs.jsx
@@ -1,6 +1,53 @@
+import "./Blogs.css"
+
 function Blogs() {
+    const blogs = [
+        {
+        id: 1,
+        title: "UI Interactions of the week",
+        date:"12 Feb 2019",
+        technology:"Express, Handlebars",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem aut ducimus similique sed est nesciunt, minus ullam facere voluptate rem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nesciunt maxime aliquid, minus autem perferendis."
+    },
+    {
+        id: 1,
+        title: "UI Interactions of the week",
+        date:"12 Feb 2019",
+        technology:"Express, Handlebars",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem aut ducimus similique sed est nesciunt, minus ullam facere voluptate rem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nesciunt maxime aliquid, minus autem perferendis."
+    },
+    {
+        id: 1,
+        title: "UI Interactions of the week",
+        date:"12 Feb 2019",
+        technology:"Express, Handlebars",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem aut ducimus similique sed est nesciunt, minus ullam facere voluptate rem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nesciunt maxime aliquid, minus autem perferendis."
+    
+    },
+        {
+        id: 1,
+        title: "UI Interactions of the week",
+        date:"12 Feb 2019",
+        technology:"Express, Handlebars",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem aut ducimus similique sed est nesciunt, minus ullam facere voluptate rem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nesciunt maxime aliquid, minus autem perferendis."
+    
+    }
+]
     return (
-        <div>Blogs</div>
+        <section className="blog">
+            <h1>Blog</h1>
+            <div className="blog-container">
+                {blogs.map((blog) => (
+                    <div className="blogs" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <h4>{blog.date} | <span className="tech">{blog.technology}</span> </h4>
+                        <p>{blog.description}</p>
+
+                        </div>
+                ))}
+            </div>
+
+        </section>
     )
 }
 
